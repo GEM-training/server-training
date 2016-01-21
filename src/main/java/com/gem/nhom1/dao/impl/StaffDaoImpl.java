266f55786f4441b9e312588b40910bf0370c4a44
Vanhop94 <hopnv.1611@gmail.com>
2016-01-21 00:54:43 -0500

    update dao

--- src/main/java/com/gem/nhom1/dao/impl/StaffDaoImpl.java
+++ src/main/java/com/gem/nhom1/dao/impl/StaffDaoImpl.java
@@ -2,32 +2,27 @@ package com.gem.nhom1.dao.impl;
 
 import com.gem.nhom1.dao.StaffDao;
 import com.gem.nhom1.model.Staff;
-import org.springframework.stereotype.Repository;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 /**
- * Created by vanhop on 1/20/16.
+ * Created by vanhop on 1/21/16.
  */
-@Repository
-@Transactional
-public class StaffDaoImpl extends AbstractDao<Integer,Staff> implements StaffDao{
-
+public class StaffDaoImpl extends AbstractDao<Integer,Staff> implements StaffDao {
     public Staff getById(int id) {
         return getByKey(id);
     }
 
     public List<Staff> getList() {
-        return getSession().;
+        return null;
     }
 
     public boolean delete(int id) {
-        return false;
-    }
-
-    public void update(Staff staff) {
-
+        try {
+            delete(getByKey(id));
+        } catch (Exception e){
+            return false;
+        }
+        return true;
     }
 }
