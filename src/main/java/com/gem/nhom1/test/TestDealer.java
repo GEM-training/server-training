23b5f4ccc3204a3f756b93757b93544f8df1209d
phuongtd <phuongtd57@gmail.com>
2016-01-24 15:18:19 +0700

    add test bill and billdetail

--- src/main/java/com/gem/nhom1/test/TestDealer.java
+++ src/main/java/com/gem/nhom1/test/TestDealer.java
@@ -1,6 +1,6 @@
 package com.gem.nhom1.test;
 
-import com.gem.nhom1.model.Dealer;
+import com.gem.nhom1.model.*;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
@@ -8,6 +8,9 @@ import org.hibernate.cfg.Configuration;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.Assert.assertEquals;
 
@@ -20,19 +23,45 @@ public class TestDealer {
     private Session session;
     private Transaction tx;
 
+    private int IdTestUpdate = 9;
+
+    private int IdTestDeleteExit = 10;
+
+    private int IdTestDeleteNotExit = 100;
+
+    private int IdTestDetail = 1;
+
     @Before
     public void setUp() {
         factory = new Configuration().configure().buildSessionFactory();
     }
 
-   // @Test
+    //@Test
     public void insert() {
         session = factory.openSession();
 
         tx = session.beginTransaction();
 
 
-        Dealer dealer = new Dealer("Test db", "Test dealer");
+        Dealer dealer = new Dealer("Insert Normal", "Insert Normal");
+
+        int id = (Integer) session.save(dealer);
+        tx.commit();
+
+        Dealer kq = (Dealer) session.get(Dealer.class, id);
+
+        Assert.assertEquals("Insert Normal", kq.getName());
+
+    }
+
+    @Test
+    public void insertNull() {
+        session = factory.openSession();
+
+        tx = session.beginTransaction();
+
+
+        Dealer dealer = new Dealer("Test db", null);
 
         int id = (Integer) session.save(dealer);
         tx.commit();
@@ -45,12 +74,31 @@ public class TestDealer {
     }
 
     @Test
+    public void insertTooLong() {
+        session = factory.openSession();
+
+        tx = session.beginTransaction();
+
+
+        Dealer dealer = new Dealer("Test dbTest dbTest dbTest dbTest dbTest dbTest dbTest dbTest dbTest dbTest dbTest dbTest dbTest dbTest dbTest dbTest dbTest dbTest dbTest dbTest dbTest dbTest dbTest dbTest dbTest dbTest dbTest dbTest dbTest dbTest dbTest db", "Address");
+
+        int id = (Integer) session.save(dealer);
+        tx.commit();
+
+        Dealer kq = (Dealer) session.get(Dealer.class, id);
+
+        Assert.assertEquals("Address", kq.getAddress());
+
+
+    }
+
+    @Test
     public void update() {
         session = factory.openSession();
 
         tx = session.beginTransaction();
 
-        Dealer dealer = (Dealer) session.get(Dealer.class, 9);
+        Dealer dealer = (Dealer) session.get(Dealer.class, IdTestUpdate);
 
         dealer.setName("New Name");
 
@@ -58,11 +106,137 @@ public class TestDealer {
 
         tx.commit();
 
-        Dealer dealer_ = (Dealer) session.get(Dealer.class, 9);
+        Dealer dealer_ = (Dealer) session.get(Dealer.class, IdTestUpdate);
+
+        Assert.assertEquals("New Name", dealer_.getName());
+
+    }
+
+    @Test
+    public void updateNull() {
+        session = factory.openSession();
+
+        tx = session.beginTransaction();
+
+        Dealer dealer = (Dealer) session.get(Dealer.class, IdTestUpdate);
+
+        dealer.setName(null);
+
+        session.save(dealer);
+
+        tx.commit();
+
+        Dealer dealer_ = (Dealer) session.get(Dealer.class, IdTestUpdate);
+
+        Assert.assertEquals("New Name", dealer_.getName());
+
+    }
+
+    @Test
+    public void updateTooLong() {
+        session = factory.openSession();
+
+        tx = session.beginTransaction();
+
+        Dealer dealer = (Dealer) session.get(Dealer.class, IdTestUpdate);
+
+        dealer.setName("too Long too Longtoo Longtoo Longtoo Longtoo Longtoo Longtoo Longtoo Longtoo Longtoo Longtoo Longtoo Longtoo Longtoo Longtoo Longtoo Longtoo Longtoo Longtoo Longtoo Longtoo Longtoo Longtoo Longtoo Longtoo Longtoo Long");
+
+        session.save(dealer);
+
+        tx.commit();
+
+        Dealer dealer_ = (Dealer) session.get(Dealer.class, IdTestUpdate);
 
         Assert.assertEquals("New Name", dealer_.getName());
 
 
+    }
+
+    @Test
+    public void deleteNotExit() {
+        session = factory.openSession();
+
+        tx = session.beginTransaction();
+
+        Dealer dealer = (Dealer) session.get(Dealer.class, IdTestDeleteNotExit);
+
+
+        session.delete(dealer);
+
+        tx.commit();
+    }
+
+    @Test
+    public void getList(){
+        session = factory.openSession();
+        tx = session.beginTransaction();
+
+        List<Dealer> dealers = session.createQuery("from Dealer").list();
+
+        Assert.assertEquals(6  , dealers.size());
+        tx.commit();
+
+    }
+
+    @Test
+    public void getUnits(){
+        session = factory.openSession();
+        tx = session.beginTransaction();
+
+        Dealer dealer = (Dealer) session.get(Dealer.class , IdTestDetail);
+
+        List<UnitDealer> listUnit = new ArrayList<UnitDealer>(dealer.getUnitDealers());
+
+
+        Assert.assertEquals(2 , listUnit.size());
+
+        tx.commit();
+
+    }
+
+    @Test
+    public void getInventories(){
+        session = factory.openSession();
+        tx = session.beginTransaction();
+
+        Dealer dealer = (Dealer) session.get(Dealer.class , IdTestDetail);
+
+        List<Inventory> inventories = new ArrayList<Inventory>(dealer.getInventorys());
+
+        Assert.assertEquals(2 , inventories.size());
+
+        tx.commit();
+
+    }
+
+    @Test
+    public void getStaffs(){
+        session = factory.openSession();
+        tx = session.beginTransaction();
+
+        Dealer dealer = (Dealer) session.get(Dealer.class , IdTestDetail);
+
+        List<Staff> staffs = new ArrayList<Staff>(dealer.getStaffs());
+
+        Assert.assertEquals(1 , staffs.size());
+
+        tx.commit();
+
+    }
+
+    @Test
+    public void getBills(){
+        session = factory.openSession();
+        tx = session.beginTransaction();
+
+        Dealer dealer = (Dealer) session.get(Dealer.class , IdTestDetail);
+
+        List<Bill> bills = new ArrayList<Bill>(dealer.getBills());
+
+        Assert.assertEquals(2  , bills.size());
+
+        tx.commit();
     }
 
 
