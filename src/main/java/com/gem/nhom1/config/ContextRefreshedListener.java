b2975b9919afbfbec357efc765559d52b8aaccd4
phuongtd57 <phuongtd57@gmail.com>
2016-01-29 16:01:13 +0700

    commit

---
+++ src/main/java/com/gem/nhom1/config/ContextRefreshedListener.java
@@ -0,0 +1,26 @@
+package com.gem.nhom1.config;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.search.Search;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ContextRefreshedListener implements ApplicationListener<ContextRefreshedEvent> {
+
+    @Autowired
+    private SessionFactory sessionFactory;
+
+    @Override
+    public void onApplicationEvent(ContextRefreshedEvent event) {
+        try {
+            Search.getFullTextSession(sessionFactory.openSession())
+                    .createIndexer().startAndWait();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("refreshed");
+    }
+}
